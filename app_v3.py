7ce86ebab769b17adebb3203f66455dc54c71985
app-stark <92332041+app-stark@users.noreply.github.com>
2026-09-09 12:01:57 -0500

    Update app_v3.py

--- app_v3.py
+++ app_v3.py
@@ -165,9 +165,9 @@ def interpretation_text(df):
 st.title("🩺 Causas mortalidad mundial GBD 2017")
 
 with st.sidebar:
-    st.header("Carga de datos")
+    st.header("")
     uploaded = st.file_uploader(
-        "Sube el dataset",
+        "",
         type=["xlsx", "xls", "csv"],
         help="La aplicación detecta automáticamente la primera fila que contiene id_causa y se adapta al número de registros.",
     )
