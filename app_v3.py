b571dc2fe9f1ebf6157d58786e5c7c6baee9c88c
app-stark <92332041+app-stark@users.noreply.github.com>
2026-09-09 11:16:12 -0500

    Update app_v3.py

--- app_v3.py
+++ app_v3.py
@@ -178,7 +178,7 @@ with st.sidebar:
     elif DEFAULT_FILE.exists():
         file_bytes = DEFAULT_FILE.read_bytes()
         filename = DEFAULT_FILE.name
-        st.info("Usando el dataset incluido en el proyecto.")
+
 
 try:
     df, header_row = load_dataframe_from_bytes(file_bytes, filename)
