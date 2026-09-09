2bbb350137d628fce3bdc5122de450cecf124337
app-stark <92332041+app-stark@users.noreply.github.com>
2026-09-09 11:18:17 -0500

    Update app_v3.py

--- app_v3.py
+++ app_v3.py
@@ -165,7 +165,7 @@ def interpretation_text(df):
 st.title("🩺 Causas de mortalidad mundial — GBD 2017")
 
 with st.sidebar:
-    st.header("Carga de datos")
+    
     uploaded = st.file_uploader(
         "Sube el dataset",
         type=["xlsx", "xls", "csv"],
